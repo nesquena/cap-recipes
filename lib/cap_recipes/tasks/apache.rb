56d2e0ca974779df1b7f88c7255c52155d6e20ef
nesquena <xgamerx10@gmail.com>
2008-06-04 22:31:37 -0700

    updated permissions repair after update code and fixed a typo in backgroundrb tasks

--- lib/cap_recipes/tasks/apache.rb
+++ lib/cap_recipes/tasks/apache.rb
@@ -1,39 +1,38 @@
 Capistrano::Configuration.instance(true).load do
-  
+
   set :apache_init_path, "/etc/init.d/apache2"
-  
+
   # ===============================================================
   # SERVER MANAGEMENT
   # ===============================================================
-  
+
   namespace :apache do
     desc "Stops the apache web server"
     task :stop, :role => :app do
       puts "Stopping the apache server"
       sudo "#{apache_init_path} stop"
     end
-    
+
     desc "Starts the apache web server"
     task :start, :role => :app do
       puts "Starting the apache server"
       sudo "#{apache_init_path} start"
     end
-    
+
     desc "Restarts the apache web server"
     task :restart, :role => :app do
       puts "Restarting the apache server"
       sudo "#{apache_init_path} restart"
     end
-    
+
     # ===============================================================
     # INSTALLATION
     # ===============================================================
-    
+
     desc 'Installs apache 2 and development headers to compile passenger'
     task :install, :roles => :web do
       puts 'Installing apache 2'
       sudo 'apt-get install apache2 apache2.2-common apache2-mpm-prefork apache2-utils libexpat1 ssl-cert libapr1 libapr1-dev   libaprutil1 libmagic1 libpcre3 libpq5 openssl apache2-prefork-dev -y'
     end
   end
-  
 end
--- lib/cap_recipes/version.rb
+++ lib/cap_recipes/version.rb
@@ -2,7 +2,7 @@ module CapRecipes #:nodoc:
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 0
-    TINY  = 4
+    TINY  = 5
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
