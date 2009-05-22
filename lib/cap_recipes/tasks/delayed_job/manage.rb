584349a6df49385dbc3bb5651310c93e755e4eb4
Nathan Esquenazi <nesquena@gmail.com>
2009-05-21 19:34:42 -0700

    refactored juggernaut and delayed_job recipes to optionally support a specific role to use

--- lib/cap_recipes/tasks/delayed_job/manage.rb
+++ lib/cap_recipes/tasks/delayed_job/manage.rb
@@ -1,26 +1,32 @@
 Capistrano::Configuration.instance(true).load do
-  set :delayed_script_path, 'script/delayed_job'
+  set :delayed_script_path, "#{current_path}/script/delayed_job"
   set :delayed_job_env, 'production'
-  set :base_ruby_path,    '/usr'
-  
+  set :delayed_job_role, :app
+  set :base_ruby_path,   '/usr'
+
   namespace :delayed_job do
     desc "Start delayed_job process"
     task :start, :roles => :app do
-      run "cd #{current_path} && #{sudo} #{base_ruby_path}/bin/ruby #{delayed_script_path} start #{delayed_job_env}"
+      with_role(delayed_job_role) do
+        try_sudo "#{base_ruby_path}/bin/ruby #{delayed_script_path} start #{delayed_job_env}"
+      end
     end
 
     desc "Stop delayed_job process"
     task :stop, :roles => :app do
-      run "cd #{current_path} && #{sudo} #{base_ruby_path}/bin/ruby #{delayed_script_path} stop #{delayed_job_env}"
+      with_role(delayed_job_role) do
+        try_sudo "#{base_ruby_path}/bin/ruby #{delayed_script_path} stop #{delayed_job_env}"
+      end
     end
 
     desc "Restart delayed_job process"
     task :restart, :roles => :app do
-      # run "cd #{current_path} && #{sudo} #{base_ruby_path}/bin/ruby #{delayed_script_path} restart #{delayed_job_env}"
-      delayed_job.stop
-      sleep(4)
-      try_sudo "killall -s TERM delayed_job; true"
-      delayed_job.start
+      with_role(delayed_job_role) do
+        delayed_job.stop
+        sleep(4)
+        try_sudo "killall -s TERM delayed_job; true"
+        delayed_job.start
+      end
     end
   end
 end
--- lib/cap_recipes/tasks/juggernaut/manage.rb
+++ lib/cap_recipes/tasks/juggernaut/manage.rb
@@ -2,46 +2,53 @@ Capistrano::Configuration.instance(true).load do
   set :juggernaut_config, "#{current_path}/config/juggernaut.yml"
   set :juggernaut_pid, "#{current_path}/tmp/pids/juggernaut.pid"
   set :juggernaut_log, "#{current_path}/log/juggernaut.log"
-  set :base_ruby_path,    '/usr'
-  
-  namespace :juggernaut do   
-             
+  set :juggernaut_role, :app
+  set :base_ruby_path,  '/usr'
+
+  namespace :juggernaut do
+
     # ===============================================================
     # PROCESS MANAGEMENT
-    # ===============================================================  
-    
+    # ===============================================================
+
     desc "Starts the juggernaut push server"
     task :start, :roles => :app do
-      puts "Starting juggernaut push server"
-      try_sudo "#{base_ruby_path}/bin/juggernaut -c #{juggernaut_config} -d --pid #{juggernaut_pid} --log #{juggernaut_log}"
+      with_role(juggernaut_role) do
+        puts "Starting juggernaut push server"
+        try_sudo "#{base_ruby_path}/bin/juggernaut -c #{juggernaut_config} -d --pid #{juggernaut_pid} --log #{juggernaut_log}"
+      end
     end
-    
+
     desc "Stops the juggernaut push server"
     task :stop, :roles => :app do
-      puts "Stopping juggernaut push server"
-      try_sudo "#{base_ruby_path}/bin/juggernaut -c #{juggernaut_config} -k * --pid #{juggernaut_pid} --log #{juggernaut_log}" 
+      with_role(juggernaut_role) do
+        puts "Stopping juggernaut push server"
+        try_sudo "#{base_ruby_path}/bin/juggernaut -c #{juggernaut_config} -k * --pid #{juggernaut_pid} --log #{juggernaut_log}"
+      end
     end
-    
+
     desc "Restarts the juggernaut push server"
     task :restart, :roles => :app do
-      juggernaut.stop
-      juggernaut.start
-    end  
-    
+      with_role(juggernaut_role) do
+        juggernaut.stop
+        juggernaut.start
+      end
+    end
+
     # ===============================================================
     # FILE MANAGEMENT
-    # ===============================================================  
-    
+    # ===============================================================
+
     desc "Symlinks the shared/config/juggernaut yaml to release/config/"
     task :symlink_config, :roles => :app do
       try_sudo "ln -s #{shared_path}/config/juggernaut.yml #{release_path}/config/juggernaut.yml"
     end
-    
+
     desc "Displays the juggernaut log from the server"
     task :tail, :roles => :app do
-      stream "tail -f #{shared_path}/log/juggernaut.log" 
+      stream "tail -f #{shared_path}/log/juggernaut.log"
     end
-    
+
   end
-  
+
 end
