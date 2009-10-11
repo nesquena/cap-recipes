490855ef3ec90425395edfe65b73d604f1ac0496
Nathan Esquenazi <nesquena@gmail.com>
2009-10-11 07:37:36 -0700

    add missing require to ruby recipe for aptitude

--- lib/cap_recipes/tasks/ruby/install.rb
+++ lib/cap_recipes/tasks/ruby/install.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../utilities')
+require File.expand_path(File.dirname(__FILE__) + '/../aptitude/manage')
 
 Capistrano::Configuration.instance(true).load do
  
