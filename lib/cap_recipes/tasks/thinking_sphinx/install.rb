370ee9f1f58119942527fea147b7fe8b4c8639da
achiu <achiu@Teumessian.local>
2009-10-26 14:15:20 -0700

    added sudo for configure for thinking_sphinx install

--- lib/cap_recipes/tasks/thinking_sphinx/install.rb
+++ lib/cap_recipes/tasks/thinking_sphinx/install.rb
@@ -9,7 +9,7 @@ Capistrano::Configuration.instance(true).load do
       utilities.apt_install 'build-essential libmysqlclient15-dev libmysql++-dev'
       run "cd /tmp; #{sudo} wget http://www.sphinxsearch.com/downloads/sphinx-0.9.8.1.tar.gz"
       run "cd /tmp; #{sudo} tar xvzf sphinx-0.9.8.1.tar.gz"
-      run "cd /tmp/sphinx-0.9.8.1/; ./configure --with-mysql-includes=/usr/include/mysql --with-mysql-libs=/usr/lib/mysql"
+      run "cd /tmp/sphinx-0.9.8.1/; #{sudo} ./configure --with-mysql-includes=/usr/include/mysql --with-mysql-libs=/usr/lib/mysql"
       run "cd /tmp/sphinx-0.9.8.1/; #{sudo} make; #{sudo} make install"
       sudo "gem install thinking-sphinx --source http://gemcutter.org --no-ri --no-rdoc"
     end
